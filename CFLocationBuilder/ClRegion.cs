d155e2bf9220528143023f399203e6b0a1147bc2
Youssef Boukeken <sbkn256@gmail.com>
2014-10-11 16:18:32 -0400

    Initial commit
    Only parsing locations supported for now.

---
+++ CFLocationBuilder/ClRegion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace CFLocationBuilder
+{
+    public class ClRegion
+    {
+        public string Name { get; set; }
+        public IEnumerable<ClSubRegion> SubRegions { get; set; }
+
+        public static IEnumerable<ClRegion> ParseRegionsFromHtml(string locationsHtml)
+        {
+            // Load the html into HTMLAgilityPack
+            // for parsing.
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(locationsHtml);
+
+            // Filter out non-relevant nodes.
+            var lastRegion = String.Empty;
+            var regions =
+                      (from rootNode in htmlDocument.DocumentNode.SelectNodes("//div[contains(@class, 'box')]")
+                       from childNode in rootNode.ChildNodes.Where(n => n.NodeType == HtmlNodeType.Element)
+                       let isH4 = childNode.Name == "h4"
+                       let isUl = childNode.Name == "ul"
+                       where isH4 || isUl
+                       // Dirty mutability... I just wanna use linq ;(
+                       // The html is formatted such that cities are preceeded by the h4 containing the "Region/City"
+                       let _ = isH4 ? (lastRegion = childNode.InnerText.Trim()) : "" 
+                       where isUl
+                       from linkNode in childNode.SelectNodes(".//li/a")
+                       let subregion = new ClSubRegion()
+                       {
+                           Name = linkNode.InnerText,
+                           RegionName = lastRegion,
+                           Url = linkNode.GetAttributeValue("href", null)
+                       }
+                       where subregion.Url != null
+                       group subregion by subregion.RegionName into region
+                       select new ClRegion()
+                       {
+                           Name = region.Key,
+                           SubRegions = region
+                       }).ToList();
+
+            return regions;
+        }
+    }
+}
---
+++ CFLocationBuilder/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CFLocationBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieve the locations from craigslist site.
+            //var baseUrl = "https://www.craigslist.org/about/sites";
+            var baseUrl = "file:///C:/Users/sef/Desktop/craigslist_dump.htm";
+            var webClient = new WebClient();
+            var locationsHtml = webClient.DownloadString(baseUrl);
+
+            var regions = ClRegion.ParseRegionsFromHtml(locationsHtml);
+
+            var targetDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var json = JsonConvert.SerializeObject(regions, Formatting.Indented);
+            var filePath = Path.Combine(targetDirectory, "cl_data.json");
+            File.WriteAllText(filePath, json);
+            Console.WriteLine(json);
+            Console.ReadLine();
+        }
+    }
+}
